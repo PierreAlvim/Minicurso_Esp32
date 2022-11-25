b7c7a15ffa679eae779c1c2dcc921cca50d9e1c6
Pierre Alvim <alvimpierre@outlook.com>
2022-11-25 16:40:32 -0300

    Wifi STA

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+const char ssid[] = "SSID";
+const char password[] = "pass";
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
   pinMode(2,OUTPUT);
 }
 
